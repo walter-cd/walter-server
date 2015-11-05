5846f3dd0d0b0a5389711d7b7a2953af145dd611
Gosuke Miyashita <gosuke-miyashita@cookpad.com>
2015-11-05 15:55:46 +0900

    Separate database related code to another file

---
+++ db.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/naoina/genmai"
+)
+
+type Db struct{}
+
+type Project struct {
+	Id        int64  `db:"pk"`
+	Name      string `size:"255"`
+	Repo      string `db:"unique" size:"255"`
+	CreatedAt *time.Time
+}
+
+type Report struct {
+	Id          int64  `db:"pk"`
+	Status      string `size:"255"`
+	ProjectId   int64
+	Branch      string `size:"255"`
+	CompareUrl  string
+	Duration    int64
+	TriggeredBy string `size:"255"`
+	CreatedAt   *time.Time
+}
+
+type Commit struct {
+	Id        int64 `db:"pk"`
+	ReportId  int64
+	Reivision string `size:"255"`
+	Author    string `size:"255"`
+	Message   string
+	CreatedAt *time.Time
+}
+
+type Stage struct {
+	Id            int64 `db:"pk"`
+	ReportId      int64
+	ParentStageId int64
+	Name          string `size:"255"`
+	Status        string `size:"255"`
+	Out           string
+	Err           string
+	Duration      int64
+	CreatedAt     *time.Time
+}
+
+func (d *Db) Init() {
+	db, err := genmai.New(&genmai.SQLite3Dialect{}, "walter.sqlite3")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	tables := [...]interface{}{
+		&Project{},
+		&Report{},
+		&Commit{},
+		&Stage{},
+	}
+
+	for _, t := range tables {
+		if err := db.CreateTable(t); err != nil {
+			panic(err)
+		}
+	}
+
+}
--- main.go
+++ main.go
@@ -6,50 +6,9 @@ import (
 	"log"
 	"net/http"
 	"path/filepath"
-	"time"
 
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/naoina/genmai"
 )
-
-type Project struct {
-	Id        int64  `db:"pk"`
-	Name      string `size:"255"`
-	Repo      string `db:"unique" size:"255"`
-	CreatedAt *time.Time
-}
-
-type Report struct {
-	Id          int64  `db:"pk"`
-	Status      string `size:"255"`
-	ProjectId   int64
-	Branch      string `size:"255"`
-	CompareUrl  string
-	Duration    int64
-	TriggeredBy string `size:"255"`
-	CreatedAt   *time.Time
-}
-
-type Commit struct {
-	Id        int64 `db:"pk"`
-	ReportId  int64
-	Reivision string `size:"255"`
-	Author    string `size:"255"`
-	Message   string
-	CreatedAt *time.Time
-}
-
-type Stage struct {
-	Id            int64 `db:"pk"`
-	ReportId      int64
-	ParentStageId int64
-	Name          string `size:"255"`
-	Status        string `size:"255"`
-	Out           string
-	Err           string
-	Duration      int64
-	CreatedAt     *time.Time
-}
 
 type templateHandler struct {
 	filename string
@@ -69,24 +28,8 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 var host = flag.String("host", ":8080", "The host of the application.")
 
 func main() {
-	db, err := genmai.New(&genmai.SQLite3Dialect{}, "walter.sqlite3")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	tables := [...]interface{}{
-		&Project{},
-		&Report{},
-		&Commit{},
-		&Stage{},
-	}
-
-	for _, t := range tables {
-		if err := db.CreateTable(t); err != nil {
-			panic(err)
-		}
-	}
+	db := &Db{}
+	db.Init()
 
 	flag.Parse() // parse the flags
 
