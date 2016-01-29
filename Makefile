f7727b044672ff41781324a116f12a413b069fcb
Gosuke Miyashita <gosukenator@gmail.com>
2016-01-29 15:44:46 +0900

    Update

--- Makefile
+++ Makefile
@@ -3,11 +3,13 @@ COMMIT = $$(git describe --always)
 deps:
 	go get -d -t ./...
 	go get github.com/jteeuwen/go-bindata/...
+	go get github.com/tools/godep
 
 test: deps
 	go test ./...
 
 build: deps
+	godep restore
 	go-bindata -pkg=assets -o=assets/assets.go ./web/...
 	go build -ldflags "-X main.GitCommit=\"$(COMMIT)\"" -o bin/walter-server
 
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/walter-cd/walter-server/db"
@@ -11,19 +12,24 @@ import (
 	"github.com/walter-cd/walter/log"
 )
 
-var host = flag.String("host", "127.0.0.1:8080", "The host of the application.")
-
 func main() {
 	db.Init()
 
-	flag.Parse() // parse the flags
+	var host string
+
+	flags := flag.NewFlagSet("walter", flag.ExitOnError)
+	flags.StringVar(&host, "host", "127.0.0.1:8080", "The host of the application.")
+
+	if err := flags.Parse(os.Args[1:]); err != nil {
+		panic(err)
+	}
 
 	r := route.GetRegexpHandler()
 	http.Handle("/", r)
 
-	log.Info(fmt.Sprintf("walter-server is listening on %s", *host))
+	log.Info(fmt.Sprintf("walter-server is listening on %s", host))
 
-	if err := http.ListenAndServe(*host, nil); err != nil {
+	if err := http.ListenAndServe(host, nil); err != nil {
 		log.Error(fmt.Sprintf("ListenAndServe: %s", err))
 	}
 }
